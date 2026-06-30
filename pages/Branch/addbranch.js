f0981caaebbce142bd5b5e0e56188a2f925dd456
paramitaNandi <paromita@onebillionideas.io>
2026-06-30 11:16:29 +0530

    test

--- pages/Branch/addbranch.js
+++ pages/Branch/addbranch.js
@@ -11,7 +11,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      // main: "#4CAF50", // green accent
+      // main: "#4CAF50", 
       main: "#0E9F6E",
     },
     background: {
